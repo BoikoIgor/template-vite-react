3d95f5b2abf1e12e46b74e15c8e68927d82053ce
Igor Boiko <igorboiko@gmail.com>
2023-09-06 17:46:32 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 33,
+      }}
+    >
+      Vite+React template
+      <img src="./images/favicon/vite.svg"></img>
+    </div>
+  );
+}
+
+export default App;
